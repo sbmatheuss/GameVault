2e6191217f0416f6c9167d14c0372836ea9cfbf7
kariel <karielemanoel.17@gmail.com>
2026-03-08 22:38:20 -0300

    fix: corrigido o error nas de rederização e navegação de rotas
    style(Home.css): adicionado novos estilos a pagina home.tsx

--- GameVault/src/App.tsx
+++ GameVault/src/App.tsx
@@ -1,7 +1,16 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router"
+import { Home } from "./pages/Home"
+
 
 function App() {
-  return (<></>)
+
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <Home />} />
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ GameVault/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+ import '../styles/Home.css'
+ export function Home() {
+   return (
+    <div>
+      <header className="header container">
+        <div className='logo'>GameVaut</div>
+        <div className='justify-align-center'>
+          <nav className='NavBar font_m c_text'>
+            <a href="#">iten</a>
+            <a href="#">iten</a>
+            <a href="#">iten</a>
+            <a href="#">iten</a>
+          </nav>
+          <div>
+            cart
+          </div>
+          <div>
+            perfil
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
