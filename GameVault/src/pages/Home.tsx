ce01f82f4028f0b063be94f9a8df643c543ca1c3
Matheus Braga <sbmatheus20@gmail.com>
2026-03-09 22:56:34 -0300

    card main feito e estilizado

--- GameVault/src/pages/Home.tsx
+++ GameVault/src/pages/Home.tsx
@@ -1,4 +1,6 @@
  import '../styles/Home.css'
+ import images from '../images/card-cyberpunk.jpg'
+
  export function Home() {
    return (
     <div>
@@ -17,8 +19,44 @@
           <div>
             perfil
           </div>
+
         </div>
       </header>
+      
+      <div className="featured-card">
+
+        <img 
+          src={images} 
+          alt="Cyberpunk 2077 Phantom Liberty"
+          className="featured-img"
+        />
+
+        <div className="featured-overlay">
+          <span className="featured-label">FEATURED RELEASE:</span>
+
+          <h2 className="featured-title">
+            CYBERPUNK 2077 - PHANTOM LIBERTY
+          </h2>
+
+          <div className="featured-info">
+            <span className="price">$69.99</span>
+            <button className="details-btn">View Details</button>
+          </div>
+        </div>
+
+      </div>
+
+
+
+          <div className='card-categories'>
+                <div><span>New Releases</span></div>
+                <div><span>Best Sellers</span></div>
+                <div><span>Pre-Orders</span></div>
+                <div><span>PC</span></div>
+                <div><span>PS5</span></div>
+                <div><span>xBOX</span></div>
+                <div><span>Nintendo Switch</span></div>
+          </div>
     </div>
   );
 }
